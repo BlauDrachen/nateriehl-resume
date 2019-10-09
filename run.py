55e430a570276d5c79d79e94c82bafb16a2520fe
Nathaniel Riehl <nate@nateriehl.com>
2019-10-09 14:50:05 -0500

    build (db): add db migrations

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 import os
-from app import create_app
+from app import create_app, db
+from app.models import Employer, Position, Task
+from flask_migrate import Migrate
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+# shell context
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, Employer=Employer, Position=Position, Task=Task)
